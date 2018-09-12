06423437a43cb1fbd217d31ce9b091527c7d138f
Ashish P <Grad57@192.168.142.128>
2018-09-12 20:51:33 +0530

    Order Class --> A base Class for the Orders Place

---
+++ src/java/com/businessLogic/Order.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.businessLogic;
+
+/**
+ *
+ * @author Ashish
+ */
+import java.util.Date;
+
+public class Order {
+
+    private String customerId;
+    private String buySell;
+    private double sharePrice;
+    private double shareQuantity;
+    private double totalPrice;
+    private String traderId; 
+    private boolean tradePlaced;
+    private Date timeStamp;
+    
+
+    public Order() {
+        super();
+    }
+
+    public Order(String customerId, String buySell, double sharePrice, double shareQuantity, double totalPrice,
+            String traderId, boolean tradePlaced, Date timeStamp) {
+        super();
+        this.customerId = customerId;
+        this.buySell = buySell;
+        this.sharePrice = sharePrice;
+        this.shareQuantity = shareQuantity;
+        this.totalPrice = totalPrice;
+        this.traderId = traderId;
+        this.tradePlaced = tradePlaced;
+        this.timeStamp = timeStamp;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public void setCustomerId(String customerId) {
+        this.customerId = customerId;
+    }
+
+    public String getBuySell() {
+        return buySell;
+    }
+
+    public void setBuySell(String buySell) {
+        this.buySell = buySell;
+    }
+
+    public double getSharePrice() {
+        return sharePrice;
+    }
+
+    public void setSharePrice(double sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+
+    public double getShareQuantity() {
+        return shareQuantity;
+    }
+
+    public void setShareQuantity(double shareQuantity) {
+        this.shareQuantity = shareQuantity;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getTraderId() {
+        return traderId;
+    }
+
+    public void setTraderId(String traderId) {
+        this.traderId = traderId;
+    }
+
+    public boolean isTradePlaced() {
+        return tradePlaced;
+    }
+
+    public void setTradePlaced(boolean tradePlaced) {
+        this.tradePlaced = tradePlaced;
+    }
+
+    public Date getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(Date timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+}
