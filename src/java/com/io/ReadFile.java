a158b5da5dc70d10ce894d235da8d9b821e00947
VishalGautamDtu <vishalgautam1994@gmail.com>
2018-09-21 11:37:37 +0530

    Functionality to read data from files added

---
+++ src/java/com/io/ReadFile.java
@@ -0,0 +1,30 @@
+package com.io;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReadFile {
+
+	public List<String> readRowsFromFile(String FILE_NAME) {
+		List<String> readRows = new ArrayList<String>();
+		
+		try {
+			InputStream inputStream = new FileInputStream(FILE_NAME);
+			InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+			BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+			String readRow;
+			while((readRow=bufferedReader.readLine())!=null) {
+				readRows.add(readRow);
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return readRows;
+	}
+}
